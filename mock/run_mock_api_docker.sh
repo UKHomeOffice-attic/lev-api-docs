a25e53d5641938bfed14e6e34c85b3e956832bfd
Robin Knipe <robin.knipe@gmail.com>
2016-10-06 16:28:13 +0100

    Ensure docker is run with super user permissions

--- mock/run_mock_api_docker.sh
+++ mock/run_mock_api_docker.sh
@@ -5,11 +5,11 @@ SCRIPT_HOME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 function build_docker_image {
     #NB: Could generalise the below to pass in the SECRET_CONTEXT env var (where client certs are pulled from in kubernetes)
     # and the TEST_URL which is the location the API being tested against. Leaving as dev for now as this is the most common use case
-    docker build -f $SCRIPT_HOME/Dockerfile -t lev-api-mock $SCRIPT_HOME;
+    sudo docker build -f $SCRIPT_HOME/Dockerfile -t lev-api-mock $SCRIPT_HOME;
 }
 
 function run_mock_api {
-    docker run \
+    sudo docker run \
       --name lev-api-mock \
       --rm \
       -p 8080:8080 \
